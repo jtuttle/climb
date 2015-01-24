7cac24bc56973cfac2ca61b13f5743fe2a324efb
John Tuttle <jtuttle@craw.cc>
2015-01-24 02:02:55 -0500

    somewhat more efficient vine generation

--- ClimbClientUnity/Assets/Code/Vine.cs
+++ ClimbClientUnity/Assets/Code/Vine.cs
@@ -8,18 +8,26 @@ public class Vine : MonoBehaviour {
     private int _platformCount = 5;
 
     private List<VineLevel> _vineLevels;
-
     private List<LeafPlatform> _leafPlatforms;
+
+	private VineLevel _lastLevel;
+	private int _firstLevelIndex;
+	private int _firstPlatformIndex;
 
 	void Awake() {
         _vineLevels = new List<VineLevel>();
         _leafPlatforms = new List<LeafPlatform>();
 
-        GameObject newLevel = UnityUtils.LoadResource<GameObject>("Prefabs/VineLevel", true);
-        _vineLevels.Add(newLevel.GetComponent<VineLevel>());
+		while(_lastLevel == null || _lastLevel.transform.position.y < Camera.main.orthographicSize + 1)
+			Grow();
+
+		_firstLevelIndex = 0;
+		_firstPlatformIndex = 0;
 	}
 	
 	void Update () {
+		transform.position -= new Vector3(0, 0.02f);
+
         if(Input.GetKey(KeyCode.Space)) {
             if(!_grow) {
                 _grow = true;
@@ -28,13 +36,27 @@ public class Vine : MonoBehaviour {
         } else {
             _grow = false;
         }
-	}
 
+		if(_lastLevel.transform.position.y < Camera.main.orthographicSize + 1)
+			Grow();
+
+		if(_vineLevels[_firstLevelIndex].transform.position.y < -Camera.main.orthographicSize - 1) {
+			VineLevel firstLevel = _vineLevels[_firstLevelIndex];
+			Destroy(firstLevel.gameObject);
+			_firstLevelIndex++;
+		}
+		
+		if(_leafPlatforms[_firstPlatformIndex].transform.position.y < -Camera.main.orthographicSize - 1) {
+			LeafPlatform firstPlatform = _leafPlatforms[_firstPlatformIndex];
+			Destroy(firstPlatform.gameObject);
+            _firstPlatformIndex++;
+        }
+    }
+    
     private void Grow() {
         float sample = (Mathf.PerlinNoise(_vineLevels.Count / 10.0f, 0) - 0.5f) * 5.0f;
 
-        VineLevel lastLevel = _vineLevels[_vineLevels.Count - 1];
-        Vector3 oldPos = lastLevel.transform.localPosition;
+		Vector3 oldPos = (_lastLevel == null ? new Vector3(0, 0) : _lastLevel.transform.localPosition);
 
         GameObject newLevelGo = UnityUtils.LoadResource<GameObject>("Prefabs/VineLevel", true);
         newLevelGo.transform.parent = transform;
@@ -43,6 +65,7 @@ public class Vine : MonoBehaviour {
         VineLevel newLevel = newLevelGo.GetComponent<VineLevel>();
 
         _vineLevels.Add(newLevel);
+		_lastLevel = newLevel;
 
         newLevel.Grow();
 
@@ -51,7 +74,6 @@ public class Vine : MonoBehaviour {
             _platformCount = 5;
         }
     }
-
 
     private void GrowPlatform(float vineHeight) {
 		float position = Random.Range(-4.0f, 4.0f);
@@ -63,5 +85,7 @@ public class Vine : MonoBehaviour {
 
 		LeafPlatform leafPlatform = leafPlatformGo.GetComponent<LeafPlatform>();
 		leafPlatform.Grow(blocks);
+
+		_leafPlatforms.Add(leafPlatform);
     }
 }
