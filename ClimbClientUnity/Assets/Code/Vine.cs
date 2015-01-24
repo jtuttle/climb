0ba3c0903751473999ae56fc924a4a9e83dfc285
John Tuttle <jtuttle@craw.cc>
2015-01-23 23:53:21 -0500

    smaller vine tiles

--- ClimbClientUnity/Assets/Code/Vine.cs
+++ ClimbClientUnity/Assets/Code/Vine.cs
@@ -5,10 +5,15 @@ using System.Collections.Generic;
 public class Vine : MonoBehaviour {
     private bool _grow = false;
 
+    private int _platformCount = 5;
+
     private List<VineLevel> _vineLevels;
+
+    private List<VinePlatform> _vinePlatforms;
 
 	void Awake() {
         _vineLevels = new List<VineLevel>();
+        _vinePlatforms = new List<VinePlatform>();
 
         GameObject newLevel = UnityUtils.LoadResource<GameObject>("Prefabs/VineLevel", true);
         _vineLevels.Add(newLevel.GetComponent<VineLevel>());
@@ -26,19 +31,31 @@ public class Vine : MonoBehaviour {
 	}
 
     private void Grow() {
-        float sample = (Mathf.PerlinNoise(_vineLevels.Count / 10.0f, 0) - 0.5f) * 10.0f;
+        float sample = (Mathf.PerlinNoise(_vineLevels.Count / 10.0f, 0) - 0.5f) * 5.0f;
 
         VineLevel lastLevel = _vineLevels[_vineLevels.Count - 1];
         Vector3 oldPos = lastLevel.transform.localPosition;
 
         GameObject newLevelGo = UnityUtils.LoadResource<GameObject>("Prefabs/VineLevel", true);
         newLevelGo.transform.parent = transform;
-        newLevelGo.transform.localPosition = new Vector3(sample, oldPos.y + 1, 0);
+        newLevelGo.transform.localPosition = new Vector3(sample, oldPos.y + 0.5f, 0);
 
         VineLevel newLevel = newLevelGo.GetComponent<VineLevel>();
 
         _vineLevels.Add(newLevel);
 
         newLevel.Grow();
+
+        if(--_platformCount == 0) {
+            GrowPlatform();
+            _platformCount = 5;
+        }
+    }
+
+
+    private void GrowPlatform() {
+        // random width
+
+        Debug.Log("PLATFORM");
     }
 }
