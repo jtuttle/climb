fae6eeb4a78266b778906e9d637ff1fe92528d72
nikkjkjk <dtuttle@umass.edu>
2015-01-24 00:52:56 -0500

    Floodwater Logic
    Water placeholder rises upon detecting input and stops when it reaches a
    certain height. Additionally a code folder was added to assets to save
    scripts to.

---
+++ ClimbClientUnity/Assets/FloodWater.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloodWater : MonoBehaviour {
+	//FIELDS
+	int x =0;
+	int y = 0;
+	private bool inputDetected = false;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//will need code raise the water 1/6 the way up the screen once there is 
+		//input detected from the user
+		
+		//need to find out what input will look like
+		if(x==0 && Input.anyKey ){
+			inputDetected =true;
+			x+=1;
+		}
+		if(y ==0 && inputDetected ==true) {
+			//raise the water to the visible part of the screen
+			transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+			if(transform.position.y >= -3.91){
+				transform.Translate(Vector3.down *Time.deltaTime, Space.World);
+				y = 1;
+			}
+		}
+	}
+	
+}
